9130511434bb4395da9c76f0eb3e2b5c6df4515d
Maria <mariaclaratimbo3@gmail.com>
2023-11-29 22:34:24 -0300

    adições

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sky-jumper
-jogo de prog
+
+## Backend
+
+Foi feito utilizando Python, Flask e SQLAlchemy
+
+## Frontend
+
+Foi feito utilizando HTML, CSS, Bootstrap e JavaScript
+
+## Jogo Sky-Jumper
+
+Foi feito utilizando Python e Pygame
--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -1,20 +1,21 @@
 export function signup(form) {
-        //var form_data = new FormData(signup_form);
-    
-        var form_data = {
-            "username": document.querySelector("#username").value,
-            "email": document.querySelector("#email").value,
-            "password": document.querySelector("#password").value
-        }
-    
-        //form_data = JSON.stringify(form_data)
+    //var form_data = new FormData(signup_form);
 
-        console.log(form_data)
+    var form_data = {
+        "username": document.querySelector("#username").value,
+        "email": document.querySelector("#email").value,
+        "password": document.querySelector("#password").value,
+        "image": document.querySelector("#image").value
+    }
 
-        fetch("http://127.0.0.1:5000/player/register", {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json",
-            body: form_data
-        });
+    //form_data = JSON.stringify(form_data)
+
+    console.log(form_data)
+
+    fetch("http://127.0.0.1:5000/player/register", {
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        body: form_data
+    });
 }
